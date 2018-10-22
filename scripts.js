7088faf4e705034f8f84cab8c2b368a9745452ae
cyanidesayonara <cyanidesayonara@gmail.com>
2018-10-22 16:21:38 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+$(document)
+  .on("click", ".ajax", function(e) {
+    e.preventDefault();
+    var url = this.href;
+    $.ajax({
+      type: "GET",
+      url: url,
+    })
+      .fail(function(xhr, ajaxOptions, thrownError) {
+        console.log(thrownError);
+      })
+      .done(function(response) {
+        $(".main").html(response);
+      })
+  })
+  .on("click", ".dropdown-content .option", function(e) {
+    e.preventDefault();
+    var selected = $(this).clone();
+    var active = $(".dropdown>.option").get(0);
+    $(".dropdown>.option").replaceWith(selected);
+    this.replaceWith(active);
+  });
