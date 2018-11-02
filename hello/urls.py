66e29ad792d089de5222229c10e35db00be3d345
cyanidesayonara <cyanidesayonara@gmail.com>
2018-11-02 13:12:20 +0200

    1

--- hello/urls.py
+++ hello/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from index import views
 
 urlpatterns = [
-    url(r"^$", views.index, name="index"),
-    url(r"^list/$", views.list, name="list"),
-    url(r"^detail/$", views.detail, name="detail"),
+    url("", views.index, name="index"),
+    url("list/", views.list, name="list"),
+    url("detail/", views.detail, name="detail"),
 ]
